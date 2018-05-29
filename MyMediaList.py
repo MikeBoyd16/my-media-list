d45cbc26abaa125a26eec5abd76b62bc6896c70d
MikeBoyd16 <mikeboyd165@gmail.com>
2018-05-28 18:09:37 -0700

    Add media record input to list area

--- MyMediaList.py
+++ MyMediaList.py
@@ -9,33 +9,25 @@ class MyListWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.my_media_list = {}
-        self.media_selection = None
 
-        # Initialize window title and dimensions
         self.title = "MyMediaList"
         self.left = 100
         self.top = 100
         self.width = 770
         self.height = 580
 
-        # Initialize widgets
         self.media_list_area = QListWidget(self)
         self.media_details_area = QTextEdit(self)
         self.add_media = QPushButton("", self)
         self.remove_media = QPushButton("", self)
         self.edit_media = QPushButton("", self)
 
-        # Initialize window
         self.init_window()
-        self.add_media = AddMedia()
-        self.select_media = SelectMedia(self.add_media)
 
     def init_window(self):
-        # Add menu
         main_menu = self.menuBar()
         file_menu = main_menu.addMenu("File")
 
-        # Add "import file" menu option
         open_file = QAction("&Import List", self)
         open_file.triggered.connect(self.import_file)
         file_menu.addAction(open_file)
@@ -44,21 +36,18 @@ class MyListWindow(QMainWindow):
         save_file.triggered.connect(self.export_file)
         file_menu.addAction(save_file)
 
-        # Set window title and dimensions
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
-        # Set widget dimensions
         self.media_list_area.setGeometry(70, 50, 250, 450)
         self.media_details_area.setGeometry(350, 50, 350, 450)
 
-        # Set button dimensions and positioning
         self.add_media.setIcon(QIcon("add_media.png"))
         self.add_media.setIconSize(QSize(24, 24))
         self.add_media.setFixedSize(50, 50)
         self.add_media.move(10, 100)
         self.add_media.setStyleSheet("QPushButton {background: #61E722;}")
-        self.add_media.clicked.connect(self.go_to_select)
+        self.add_media.clicked.connect(self.add_media_record)
 
         self.remove_media.setIcon(QIcon("remove_media.png"))
         self.remove_media.setIconSize(QSize(24, 24))
@@ -72,35 +61,46 @@ class MyListWindow(QMainWindow):
         self.edit_media.move(10, 260)
         self.edit_media.setStyleSheet("QPushButton {background: #E7E122;}")
 
-        # Disable text editing
         self.media_details_area.setDisabled(True)
 
         self.setStyleSheet("QMainWindow {background: #BCBCBC;}")
+
+    def update_list(self):
+        self.media_list_area.clear()
+        for key in self.my_media_list:
+            self.media_list_area.addItem("[" + self.my_media_list[key]["Media"] + "] " +
+                                         self.my_media_list[key]["Name"])
 
     def import_file(self):
         file_name = QFileDialog.getOpenFileName(self, "Import File")
         file = open(file_name[0], "r")
         self.my_media_list = json.load(file)
+        self.update_list()
 
     def export_file(self):
         file_name = QFileDialog.getSaveFileName(self, "Import File")
         file = open(file_name[0], "w")
         json.dump(self.my_media_list, file)
 
-    def go_to_select(self):
-        self.select_media.show()
+    def add_media_record(self):
+        select_media = SelectMedia()
+        select_media.show()
+        select_media.exec_()
+        self.my_media_list[select_media.get_media_selection() + "-" +
+                           select_media.temp_input["Name"]] = select_media.temp_input
+        self.update_list()
 
 
 class SelectMedia(QDialog):
-    def __init__(self, add_media):
+    def __init__(self):
         super().__init__()
+        self.temp_input = {}
         self.title = "Media Select"
         self.left = 100
         self.top = 100
         self.width = 200
         self.height = 200
 
-        self.add_media = add_media
         self.select_media_combo = QComboBox(self)
         self.ok = QPushButton("OK", self)
 
@@ -121,21 +121,25 @@ class SelectMedia(QDialog):
         self.setStyleSheet("QMainWindow {background: #BCBCBC;}")
         self.setGeometry(self.left, self.top, self.width, self.height)
         self.ok.move(50, 50)
-        self.ok.clicked.connect(self.close_dialog)
+        self.ok.clicked.connect(self.go_to_add_media)
         self.select_media_combo.addItems(["Movie", "TV", "Music", "Book", "Video Game"])
         self.init_layout()
 
     def get_media_selection(self):
         return self.select_media_combo.currentText()
 
-    def close_dialog(self):
-        self.add_media.show()
+    def go_to_add_media(self):
+        add_media_record = AddMedia()
+        add_media_record.exec_()
+        self.temp_input = add_media_record.temp_input
+        self.temp_input["Media"] = self.select_media_combo.currentText()
         self.hide()
 
 
 class AddMedia(QDialog):
     def __init__(self):
         super().__init__()
+        self.temp_input = {}
         self.labels = {"Type": QLabel(), "Favorite": QLabel(), "Name": QLabel(),
                        "Artist": QLabel(), "Album": QLabel(), "Duration": QLabel(),
                        "Genres": QLabel(), "Record Labels": QLabel(), "Producers": QLabel()}
@@ -184,9 +188,15 @@ class AddMedia(QDialog):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
         self.setStyleSheet("QMainWindow {background: #BCBCBC;}")
+        self.submit.clicked.connect(self.submit_media)
         self.init_layout()
 
     def submit_media(self):
+        for key in self.inputs:
+            if isinstance(self.inputs[key], QComboBox):
+                self.temp_input[key] = self.inputs[key].currentText()
+            else:
+                self.temp_input[key] = self.inputs[key].text()
         self.hide()
 
 
